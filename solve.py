0d0a0cc7b902d4fca8ab64fa1887b78fcfe1a71a
schonwetter <alexandre.bontems@gmail.com>
2018-02-20 18:20:44 +0100

    backtrack complete

--- solve.py
+++ solve.py
@@ -1,63 +1,101 @@
 from gen import generate
 from ipdb import set_trace
+from random import choice
 
 
 def solve_backtrack(instance):
-    current_actor_idx = 0
-    allocation = {a: None for a in range(len(instance))}
+    """Try to find a Localy Envy Free allocation for the given instance with a 
+    backtracking algorithm.
+
+    Args:
+        instance(list list int): ordered lists of preferences for each agent.
+
+    Returns:
+        Resulting allocation if it exists ({actor: object allocated})
+        False if no solution can be found.
+    """
+
+    # Current actor index. Starts at 0.
+    ca_idx = 0
+    # Dict containing current allocation.
+    alloc = {a: None for a in range(len(instance))}
+    # Keeps track of what object was allocated to who and could  not satisfy 
+    # the problem.
     impossible_alloc = {a: list() for a in range(len(instance))}  
-    taken_resources = list()    
+    # List of currently allocated resources for the current allocation.
+    allocated_resources = list()    
     
-    #set_trace()
+    # set_trace()
     
-    while not allocation[len(instance)-1]:
-        # get available resources in order of 
-        # pref for current agent
-        agent_prefs = instance[current_actor_idx]
-        imp_alloc = impossible_alloc[current_actor_idx]
-        available = list()
-        for r in agent_prefs:
-            if r not in taken_resources and r not in imp_alloc:
-                available.append(r)
+    # As long as the last agent has not been allocated a resources, the problem
+    # has not been solved.
+    while not alloc[len(instance)-1] != None:
+        # get available resources in order of preference for current agent
+        agent_prefs = instance[ca_idx]
+        imp_alloc = impossible_alloc[ca_idx]
+        available = [r for r in agent_prefs \
+            if r not in allocated_resources and r not in imp_alloc]
                
-        # try to allocate available resources
+        # try to allocate one of the available resources
         allocation_happened = False
         for r in available:
-            # no constraints for first actor
-            if current_actor_idx == 0:
-                allocation[current_actor_idx] = r
-                taken_resources.append(r)
-                current_actor_idx += 1
+            # no constraints for first actor, pick first available resource
+            if ca_idx == 0:
+                alloc[ca_idx] = r
+                allocated_resources.append(r)
+                ca_idx += 1
                 allocation_happened = True
                 break
-            # check constraints with resource `r`
-            r_idx_current_agent = instance[current_actor_idx].index(r)
-            idx_in_previous_agent = instance[current_actor_idx].index(allocation[current_actor_idx-1])
-            r_idx_in_previous_agent = instance[current_actor_idx-1].index(r)
-            idx_in_previous_agent2 = instance[current_actor_idx-1].index(allocation[current_actor_idx-1])
+
+            # else, check constraints with resource `r`
+            # index of `r` in the list of prefs of the current agent
+            r_idx_current_agent = instance[ca_idx].index(r)
+            # index of the resource given to the previous agent in the list of
+            # prefs of the current agent
+            idx_in_previous_agent = instance[ca_idx].index(alloc[ca_idx-1])
+            # index of `r` in the list of prefs of the previous agent
+            r_idx_in_previous_agent = instance[ca_idx-1].index(r)
+            # index of the resource given to the previous agent in the list of
+            # prefs of the previous agent
+            idx_in_previous_agent2 = instance[ca_idx-1].index(alloc[ca_idx-1])
             
-            if idx_in_previous_agent < r_idx_current_agent and r_idx_in_previous_agent < idx_in_previous_agent2:
-                allocation[current_actor_idx] = r
-                taken_resources.append(r)
-                current_actor_idx += 1
+            if idx_in_previous_agent > r_idx_current_agent and r_idx_in_previous_agent > idx_in_previous_agent2:
+                alloc[ca_idx] = r
+                allocated_resources.append(r)
+                ca_idx += 1
                 allocation_happened = True
                 break
             
         if not allocation_happened:
-            current_actor_idx -= 1
-            impossible_alloc[current_actor_idx].append(allocation[current_actor_idx])
-            taken_resources.remove(allocation[current_actor_idx])
-            allocation[current_actor_idx] = None
+            ca_idx -= 1
+            impossible_alloc[ca_idx].append(alloc[ca_idx])
+            allocated_resources.remove(alloc[ca_idx])
+            alloc[ca_idx] = None
 
-        if current_actor_idx == 0 and len(impossible_alloc[current_actor_idx]) == len(instance):
+        if ca_idx == 0 and len(impossible_alloc[ca_idx]) == len(instance):
             # unsolvable instance
             return False
             
-    return allocation
+    return alloc
+
+
+def pprint_instance(instance, allocation=None):
+    if not allocation: allocation = dict()
+    n = len(instance)
+    
+    print("\n")
+    for i in range(n):
+        buffer = "\t"
+        for a in range(n):
+            obj = instance[a][i]
+            buffer += "{}{}\t".format("*" if obj == allocation.get(a) else " ", obj)
+        print(buffer)
     
     
 if __name__ == "__main__":
-    #instance = generate(3)[4]
-    instance = [[0, 1, 2], [1, 0, 2], [2, 0, 1]]
-    print(instance)
-    print("Allocation: {}".format(solve_backtrack(instance)))
+    instances = generate(3)
+    for _ in range(10):
+        instance = choice(instances)
+        alloc = solve_backtrack(instance)
+        pprint_instance(instance, allocation=alloc)
+    # instance = [[0, 1, 2], [1, 0, 2], [2, 0, 1]]

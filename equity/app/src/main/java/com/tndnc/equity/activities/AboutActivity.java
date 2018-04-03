08e5e24a051d7f5830854d84f1a3b3b4166b88a5
Alexandre Bontems <alexandre.bontems@gmail.com>
2018-04-03 16:28:49 +0200

    about page, wip

---
+++ equity/app/src/main/java/com/tndnc/equity/activities/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.tndnc.equity.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tndnc.equity.R;
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
